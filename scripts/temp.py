b352f32d5e0034a73cb248fb341be002bcebf932
atrasyonda <atrasfarizi02@gmail.com>
2024-03-05 23:04:03 +0700

    menambahkan program lmi

--- scripts/temp.py
+++ scripts/temp.py
@@ -29,10 +29,9 @@ for i, element in enumerate(Ac_pk):
     # print(f"Indeks {i}: {element}")
     Ai = element
     # print ("====== ITERASI KE -", i+1 , " ======")
-    Z = Ai@Y+B@Wi
     lmi = cp.vstack([
-        cp.hstack([Y, Z.T ,Y, Wi.T]), #baris 1
-        cp.hstack([Z, Y, np.zeros([3,3]), np.zeros([3,2])]), #baris 2
+        cp.hstack([Y, (Ai@Y+B@Wi).T ,Y, Wi.T]), #baris 1
+        cp.hstack([Ai@Y+B@Wi, Y, np.zeros([3,3]), np.zeros([3,2])]), #baris 2
         cp.hstack([Y, np.zeros([3,3]), invQts, np.zeros([3,2])]), #baris 3
         cp.hstack([Wi, np.zeros([2,3]), np.zeros([2,3]), invRts]) #baris 4
         ])
@@ -46,7 +45,8 @@ for i, element in enumerate(Ac_pk):
         # print("Optimal value: ", problem.value)
         # print("Y = ", Y.value)
         # print("Wi = " , Wi.value)
-        Ki = Wi.value@np.linalg.inv(Y.value)
+        P = np.linalg.inv(Y.value)
+        Ki = Wi.value@P
         # print("Ki = ", Ki)
         outputKi.append(Ki)
     else:
@@ -54,23 +54,40 @@ for i, element in enumerate(Ac_pk):
         print("Status:", problem.status)
 # print ("Output Ki : ", outputKi)
 print("Dimensi Ki : ", np.shape(outputKi))
-print("Dimensi Ki.T", np.shape(outputKi.T))
+print("Dimensi P : ", np.shape(P))
+
 #================================================================================================
 
-Q = 0.9*np.diag([0.33, 0.33, 0.33])
-R = 0.1*np.diag([0.8, 0.2])
-N = 20
-u_bar= np.array([1.4, 20])
+Q = 0.9*np.diag([0.33, 0.33, 0.33]) # state weight matrix Q 3x3
+R = 0.1*np.diag([0.8, 0.2]) # input weight matrix R 2x1
+N = 20 # horizon
+u_bar= np.array([[1.4], [20]]) # matrix 2x1
+u_bar_squared= u_bar@u_bar.T 
+Z = cp.Variable((n, n), symmetric=True)
 
-print (u_bar)
-u_bar_squared= u_bar@u_bar
-print(u_bar_squared)
-Z = cp.Variable()
+delta_u = np.ones((m, 1, N)) # 2x1x20
+x = np.ones((n,1, N+1)) # 3x1x21
 
+# print (np.shape(x[:, 0]))
+
+# matrix_3d = np.random.rand(m, 1, N+1)
+# print("Matrix 3D:")
+# print(matrix_3d[:,:,1])
+# print("Shape:", matrix_3d[:,:,1].shape)
 
 def cost_function(delta_u, x, Q, R, P, N): # Jk for MPC cost function
     cost = 0
     for i in range(N):
-        cost += x[:, i].T@(Q@x[:, i]) + delta_u[i].T@(R @ delta_u[i])
-    cost += x[:, N].T @(P@x[:, N])
+        cost += x[:,:, i].T@(Q@x[:,:, i]) + delta_u[:,:,i].T@(R @ delta_u[:,:,i])
+    cost += x[:,:,N].T @(P@x[:,:, N])
     return cost
+
+
+Jk = cost_function(delta_u, x, Q, R, P, N)
+print ("Jk = ", Jk)
+lmi2 = cp.vstack([
+    cp.hstack([-Z, Z@(Ai+B@Ki).T]), #baris 1
+    cp.hstack([(Ai+B@Ki)@Z, -Z]), #baris 2
+    ])
+constraints2 = [lmi2<<0 , Ki@Z@Ki.T-u_bar_squared<<0] 
+
